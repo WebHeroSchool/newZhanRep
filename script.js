983c122b5d4251cd7cae7a6b837ef6d3d734e9d3
Жанат Сарбасов <zhan.sarbasov@yandex.ru>
2020-09-02 20:38:17 +0300

    изменение в скрипте: проверены отступы и изменена ссылка по имени

--- script.js
+++ script.js
@@ -2,34 +2,32 @@ let body = document.body;
 let url = window.location.toString();
 
 const getNameFromUrl = (url) => {
-  let getUrl = url.split('=');
-  let name = getUrl[1];
-  if(name == undefined) {
-  name = 'Zhan30';
-  }
-return name;
+    let getUrl = url.split('=');
+    let name = getUrl[1];
+    if(name == undefined) {
+        name = 'Zhan30';
+    }
+    return name;
 }
+  
 
 fetch(`https://api.github.com/users/${getNameFromUrl(url)}`)
     .then(res => res.json())
     .then(json => {
-        console.log(json.avatar_url);
-        console.log(json.name);
-        console.log(json.bio);
-        console.log(json.html_url);
         let avatar = new Image();
         avatar.src = json.avatar_url;
         body.append(avatar);
-        let name = document.createElement('h2');
+        
+        let name = document.createElement('h3');
         if (json.name != null) {
             name.innerHTML = json.name;
         } else {
             name.innerHTML = 'Пользователь не найден';
         }
         body.append(name);
-        name.addEventListener("click", () => window.location = 'https://webheroschool.github.io/newZhanRep/');
+        name.addEventListener("click", () => window.location = 'https://github.com/WebHeroSchool/newZhanRep');
 
-        let bio = document.createElement('h2');
+        let bio = document.createElement('h4');
         if (json.bio != null) {
             bio.innerHTML = json.bio;
         } else {
